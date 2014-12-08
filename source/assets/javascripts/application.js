197b5fdb2760c20b803ed745fa5bba7022ee936b
John Wilson <john@j-wilson.com>
2014-12-07 20:43:04 -0500

    Add navigation toggle javascript

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,10 +1,22 @@
-// *************************************
+// -------------------------------------
 //
-//   Baseman
-//   -> Manifest file
+//   Navigation
+//   -> Controls navigation toggle
 //
-// *************************************
+// -------------------------------------
 
-// -------------------------------------
-//   Navigation Toggle
-// -------------------------------------
+function toggleNav(event) {
+  var clicked = event.target;
+  var nav = document.getElementsByClassName('navigation')[0];
+  var toggleA = document.getElementsByClassName('navigation-toggle')[0];
+  var toggleB = document.getElementsByClassName('navigation-toggle')[1];
+
+  if (clicked == toggleA || clicked == toggleB) {
+    nav.classList.toggle('is-shown');
+    event.preventDefault();
+  }
+};
+
+window.onclick = function(event) {
+  toggleNav(event);
+};
