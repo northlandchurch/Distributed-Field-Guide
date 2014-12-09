72fe1c12928c2c6e9da2d953a7eebdf4b51e6181
John Wilson <john@j-wilson.com>
2014-12-08 21:27:23 -0500

    Update config.rb to help with build, etc.

--- config.rb
+++ config.rb
@@ -1,29 +1,64 @@
-activate :automatic_image_sizes
-activate :directory_indexes
+# --------------------------------------
+#   Config
+# --------------------------------------
+
+# ----- Middleman ----- #
+
 activate :livereload
 
-activate :autoprefixer do |config|
-  config.browsers = ['last 2 versions', 'Explorer >= 9']
-  config.cascade = false
-end
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-ignore 'assets/javascripts/vendor/*'
+# ----- Assets ----- #
 
 set :css_dir, 'assets/stylesheets'
 set :js_dir, 'assets/javascripts'
 set :images_dir, 'assets/images'
 set :fonts_dir, 'assets/fonts'
 
-# Build-specific configuration
+# ----- Images ----- #
+
+activate :automatic_image_sizes
+
+# --------------------------------------
+#   Content
+# --------------------------------------
+
+# ----- Blog ----- #
+
+activate :blog do |blog|
+end
+
+# ----- Directories for assets (needs to be activated after blog) ----- #
+
+activate :directory_indexes
+
+# --------------------------------------
+#   Production
+# --------------------------------------
+
+# ----- Prefixing ----- #
+
+activate :autoprefixer do |config|
+  config.browsers = ['last 2 versions', 'Explorer >= 9']
+  config.cascade = false
+end
+
+# ----- Build ----- #
+
+ignore 'assets/fonts/*'
+
 configure :build do
   activate :minify_css
   activate :minify_javascript
   activate :relative_assets
+  activate :gzip
+  activate :move
 end
+
+# Moves required Typography.com directory untouched
+
+class Move < Middleman::Extension
+  def after_build
+    FileUtils.cp_r('source/assets/fonts/', 'build/assets')
+  end
+end
+
+::Middleman::Extensions.register(:move, Move)
