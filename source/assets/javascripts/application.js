a3347f0f0e956199efc81e1f65d5f38b8538dd4e
John Wilson <john@j-wilson.com>
2014-12-20 21:56:41 -0500

    Add scrolling animatino to overview page for links

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -5,6 +5,25 @@
 //
 // -------------------------------------
 
+function smoothScroll() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+}
+
+window.onload = function(event) {
+  smoothScroll();
+};
+
 function toggleNav(event) {
   var clicked = event.target;
   var nav = document.getElementsByClassName('navigation')[0];
