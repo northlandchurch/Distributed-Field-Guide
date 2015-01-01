88a4a28a41810600eb6ae659d6b1e314202b6291
John Wilson <john@j-wilson.com>
2015-01-01 16:21:46 -0500

    Make navigation toggle work for IE9 by switch to jQuery

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -31,18 +31,8 @@ window.onload = function(event) {
 //
 // -------------------------------------
 
-function toggleNav(event) {
-  var clicked = event.target;
-  var nav = document.getElementsByClassName('navigation')[0];
-  var toggleA = document.getElementsByClassName('navigation-toggle')[0];
-  var toggleB = document.getElementsByClassName('navigation-toggle')[1];
-
-  if (clicked == toggleA || clicked == toggleB) {
-    nav.classList.toggle('is-shown');
-    event.preventDefault();
-  }
-};
-
-window.onclick = function(event) {
-  toggleNav(event);
-};
+$(function() {
+  $(".navigation-toggle").on("click", function() {
+    $(".navigation").toggleClass("is-shown");
+  });
+});
